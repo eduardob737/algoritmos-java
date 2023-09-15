8ebbe582fcd1d7beb79e6cd0d090d5312e5d06c9
Eduardo Barbosa <eduardobarbosalhp@gmail.com>
2023-09-14 22:34:21 -0300

    novos exercicios ate 32

---
+++ src/br/com/eduardobarbosa/algoritmosjava/programacaoestruturada/arrays/Exc32.java
@@ -0,0 +1,34 @@
+package br.com.eduardobarbosa.algoritmosjava.programacaoestruturada.arrays;
+
+import br.com.eduardobarbosa.algoritmosjava.programacaoestruturada.Utils;
+
+public class Exc32 {
+    // Criar um vetor A com 5 elementos inteiros. Escreva um programa que
+    // imprima a tabuada de cada um dos elementos do vetor A.
+
+    public static void main(String[] args) {
+
+        int[] vetorA = new int[5];
+
+        System.out.print("Vetor A: ");
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] = Utils.random.nextInt(0, 101);
+
+            if (i == 0) {
+                System.out.print(vetorA[i]);
+            } else {
+                System.out.print(", " + vetorA[i]);
+            }
+        }
+
+        System.out.println("\n");
+
+        for (int elemento : vetorA) {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(elemento + " x " + i + " = " + elemento * i);
+            }
+            System.out.println("-------------------");
+        }
+    }
+}
